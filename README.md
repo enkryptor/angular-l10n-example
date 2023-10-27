6abccdb516a3da13e3fc2d942a26b236949adb28
enkryptor <enkryptor@yandex.ru>
2023-10-27 15:49:38 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Выгрузить переводы
+
+Запустить в папке проекта:
+```
+ng extract-i18n --format json --out-file ru.json
+```
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { loadTranslations } from '@angular/localize';
+
+import { AppComponent } from './app.component';
+
+async function initializeApp(): Promise<any> {
+  const response = await fetch('/ru.json');
+  const { translations } = await response.json();
+  loadTranslations(translations);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: () => initializeApp,
+    multi: true
+   }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
